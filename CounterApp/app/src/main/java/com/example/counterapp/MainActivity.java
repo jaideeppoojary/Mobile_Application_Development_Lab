b864b090371efaf85efdd0cd2cc2020e8e02cf76
Jaideep <jaideep.jaidee@gmail.com>
2022-07-19 14:24:26 +0530

    Counter Application

---
+++ CounterApp/app/src/main/java/com/example/counterapp/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.counterapp;
+//    Jaideep 4SN19CS035
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Thread";
+    Button start;
+    Button stop;
+    TextView counterText;
+    Switch reverseSwitch;
+    int counter;
+    int counterStep;
+    boolean running = true;
+
+    Handler mainHandler = new Handler();
+
+
+    public void myInit(){
+        start= (Button) findViewById(R.id.buttonToStart);
+        stop= (Button) findViewById(R.id.buttonToStop);
+        counterText= (TextView) findViewById(R.id.textViewCounter);
+        reverseSwitch = (Switch) findViewById(R.id.switchToReverse);
+        counter = 1;
+        counterStep =1;
+    }
+    public void startThread(){
+        myThread obj = new myThread();
+        obj.start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myInit();
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter =0;
+                running = true;
+                startThread();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                running = false;
+            }
+        });
+    }
+
+    class myThread extends  Thread {
+        @Override
+        public  void run(){
+            while(running){
+                counterStep=1;
+                if(reverseSwitch.isChecked())
+                    counterStep=-1;
+                counter+=counterStep;
+
+                mainHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        counterText.setText(counter+"");
+                    }
+                });
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+
+}
